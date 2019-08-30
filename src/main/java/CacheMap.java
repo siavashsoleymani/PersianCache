05b60784471231d0878ef37dfdd6cd4761f8e4eb
siavash <siavash.solimanii@yahoo.com>
2019-08-30 17:11:48 +0430

    improvement on messaging

--- src/main/java/CacheMap.java
+++ src/main/java/CacheMap.java
@@ -29,7 +29,7 @@ public class CacheMap extends HashMap<String, String> {
         return super.put(key, value);
     }
 
-    public String networkRemove(Object key) {
+    public String networkR1emove(Object key) {
         return super.remove(key);
     }
 }
